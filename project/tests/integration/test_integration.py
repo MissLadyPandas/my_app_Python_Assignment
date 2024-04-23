ccc1345288910c97464b785ded8752426caeb490
Katharine Brumback <kbrumback@ivytech.edu>
2024-04-23 18:15:59 -0400

    Add files via upload

---
+++ project/tests/integration/test_integration.py
@@ -0,0 +1,9 @@
+import unittest
+
+class TestIntegration(unittest.TestCase):
+    def test_integration(self):
+        """Test some integrated set of actions"""
+        self.assertTrue(True)  # Replace with real tests
+
+if __name__ == '__main__':
+    unittest.main()
